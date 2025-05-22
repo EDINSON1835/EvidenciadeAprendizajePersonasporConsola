7886d7e3b9e33a361dd0573b8d233d3263d44d5f
Edinson Mena Arroyo <edinson.menaa@est.iudigital.edu.co>
2025-05-22 08:49:15 -0500

    Agrega método para mostrar nombre y género

--- Main.java
+++ Main.java
@@ -7,18 +7,29 @@ public class Main {
     private static final ArrayList<Persona> personas = new ArrayList<>();
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void capturarPersonas() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("\nIngrese los datos de la persona " + (i + 1));
-            System.out.print("Nombre: ");
-            String nombre = scanner.nextLine();
-            System.out.print("Apellido: ");
-            String apellido = scanner.nextLine();
-            System.out.print("Género (Masculino/Femenino): ");
-            String genero = scanner.nextLine();
-            System.out.print("Edad: ");
-            int edad = Integer.parseInt(scanner.nextLine());
-
-            personas.add(new Persona(nombre, apellido, genero, edad));
-        }
+    public static void main(String[] args) {
+        capturarPersonas();
+        mostrarNombresYGeneros();
+        System.out.println("Promedio de edades: " + calcularPromedioEdad());
+        System.out.println("Cantidad de personas masculinas: " + contarGenero("Masculino"));
+        System.out.println("Cantidad de personas femeninas: " + contarGenero("Femenino"));
     }
+
+     public static double calcularPromedioEdad() {
+        int suma = 0;
+        for (Persona persona : personas) {
+            suma += persona.getEdad();
+        }
+        return (double) suma / personas.size();
+    }
+
+    public static int contarGenero(String generoBuscado) {
+        int contador = 0;
+        for (Persona persona : personas) {
+            if (persona.getGenero().equalsIgnoreCase(generoBuscado)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+}
